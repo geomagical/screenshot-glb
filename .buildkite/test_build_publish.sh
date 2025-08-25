479b39064f6a3baed05cd2288548ce572a053ba4
Benn Herrera <benn@geomagical.com>
2025-08-25 14:06:39 -0700

    CI and packaging.

---
+++ .buildkite/test_build_publish.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+SCRIPT_DIR=$(dirname "${0}")
+SCRIPT_DIR=$(cd "${SCRIPT_DIR}"; pwd)
+SRC_DIR=$(cd "${SCRIPT_DIR}"; cd ..; pwd)
+PACKAGE_DIR="${SRC_DIR}/dist"
+
+cd "${SRC_DIR}"
+
+npm test
+
+npx tsc
+npm pack
+
+cp -v "${SCRIPT_DIR}/package.nmprc" "${PACKAGE_DIR}/.npmrc"
+
+case "${BUILDKITE_BRANCH}" in
+main) DRY_RUN=;;
+*) DRY_RUN="--dry-run";;
+esac
+
+# TODO: set up secret for CI
+echo npx google-artifactregistry-auth --repo-config="${PACKAGE_DIR}/.npmrc" --credential-config="${HOME}/.npmrc"
+
+# TODO: get this to actually work
+(cd ${PACKAGE_DIR} && echo npm publish ${DRY_RUN})
+
